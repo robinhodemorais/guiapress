d4f41fac1c7c6ed273fab7bcfa5c8fb69aebd2be
Robinho de Morais <robinhodemorais@gmail.com>
2021-01-24 20:46:53 -0300

    Base do Projeto

---
+++ database/database.js
@@ -0,0 +1,7 @@
+const Sequelize = require("sequelize");
+const connection = new Sequelize('guiapress','root','123456',{
+    host:'localhost',
+    dialect:'mysql'
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//View engine
+app.set('view engine', 'ejs');
+
+//Configuração para o express trabalhar com os arquivos staticos
+//css, html, js e etc
+app.use(express.static("public"));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//DataBase
+connection
+  .authenticate()
+  .then(() => {
+      console.log("Conexão feita com sucesso");
+  }).catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+    //renderiza a view view\index.ejs
+    res.render("index");
+});
+
+app.listen(8080,() => {
+    console.log("O servidor está rodando na porta 8080");
+});
