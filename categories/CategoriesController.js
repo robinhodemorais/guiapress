ca52acb8a11abc1707c02576b0a7fb04ef2b9ab9
Robinho de Morais <robinhodemorais@gmail.com>
2021-01-26 23:00:16 -0300

    Deletando Categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -34,4 +34,23 @@ router.get("/admin/categories",(req, res) => {
     });
 });
 
+router.post("/categories/delete",(req,res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+           Category.destroy({
+               where: {
+                   id:id
+               }
+           }).then(() =>{
+            res.redirect("/admin/categories");
+           });
+        } else {//se não for numero
+            res.redirect("/admin/categories");
+        }
+    } else {//null
+        res.redirect("/admin/categories");
+    }
+});
+
 module.exports = router;
