ed8934f171e89144eeefae01c5c2874daa25fcdc
Robinho de Morais <robinhodemorais@gmail.com>
2021-01-28 21:05:55 -0300

    Salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res.send("Rota de Artigos");
@@ -12,5 +14,18 @@ router.get("/admin/articles/new", (req, res) => {
     });
     
 });
+
+router.post("/articles/save",(req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title:title,
+        slug:slugify(title),
+        body:body,
+        categoryId:category
+    })
+})
 
 module.exports = router;
