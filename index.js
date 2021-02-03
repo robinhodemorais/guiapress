c855593de3dd58a07da19346feaf9c602924a1c9
Robinho de Morais <robinhodemorais@gmail.com>
2021-02-02 22:35:31 -0300

    Filtrando artigos por categoria

--- index.js
+++ index.js
@@ -71,6 +71,26 @@ app.get("/:slug",(req,res) => {
   });
 });
 
+app.get("/category/:slug",(req,res) => {
+  var slug = req.params.slug;
+  Category.findOne({
+    where: {
+      slug:slug
+    },
+    include:[{model:Article}]
+  }).then(category => {
+    if (category != undefined ) {
+      Category.findAll().then(categories => {
+        res.render("index",{articles:category.articles, categories:categories});
+      });
+    } else {
+      res.rendiret("/");
+    }
+  }).catch( err => {
+    res.rendiret("/");
+  });
+});
+
 app.listen(8080,() => {
     console.log("O servidor está rodando na porta 8080");
 });
